b1a780d7be03da8f755c10264337c8f6c949dcc2
wyang19a <wyang19a@gmail.com>
2020-12-16 07:38:25 -0500

    Add stuff back in

--- config.js
+++ config.js
@@ -4,11 +4,10 @@ module.exports = {
   url: 'https://wootae-blog.netlify.com',
   pathPrefix: '/',
   title: 'Wootae Yang',
-  subtitle: 'Software Developer, MCSA: SQL Database development',
+  subtitle: 'Software Developer, MCSA: Database development',
   copyright: '© All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
   useKatex: false,
   menu: [
     {
@@ -25,15 +24,26 @@ module.exports = {
     }
   ],
   author: {
-    name: 'Wootae Yang',
+    name: 'John Doe',
     photo: '/photo.jpg',
-    bio: 'Software Developer, MCSA: SQL Database development',
+    bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
       email: 'wyang19a@gmail.com',
+      facebook: '#',
+      telegram: '#',
       twitter: '#',
       github: 'wyang19a',
+      rss: '',
+      vkontakte: '',
       linkedin: 'wootae-yang',
       instagram: '#',
+      line: '',
+      gitlab: '',
+      weibo: '',
+      codepen: '',
+      youtube: '',
+      soundcloud: '',
+      medium: '',
     }
   }
 };
